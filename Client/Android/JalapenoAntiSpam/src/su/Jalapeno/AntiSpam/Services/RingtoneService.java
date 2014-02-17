3abcabf97b187f5712400bfa7685b46268336300
Alexander <lexruster@gmail.com>
2014-02-17 16:44:57 +0400

    +

---
+++ Client/Android/JalapenoAntiSpam/src/su/Jalapeno/AntiSpam/Services/RingtoneService.java
@@ -0,0 +1,17 @@
+package su.Jalapeno.AntiSpam.Services;
+
+public class RingtoneService {
+	private SettingsService _settingsService;
+
+	public RingtoneService(SettingsService settingsService) {
+		_settingsService = settingsService;
+	}
+
+	public void ContactRingtone() {
+
+	}
+
+	public void EmulateIncomeSms() {
+
+	}
+}
