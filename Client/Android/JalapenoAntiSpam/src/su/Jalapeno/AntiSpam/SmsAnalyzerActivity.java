aae794ebb842474b1b965bbda1ec920c0ba820a2
alexus_ru <alexus_ru@localhost>
2014-02-19 19:05:04 +0400

    ???? ???

--- Client/Android/JalapenoAntiSpam/src/su/Jalapeno/AntiSpam/SmsAnalyzerActivity.java
+++ Client/Android/JalapenoAntiSpam/src/su/Jalapeno/AntiSpam/SmsAnalyzerActivity.java
@@ -1,11 +1,22 @@
 package su.Jalapeno.AntiSpam;
 
+import com.google.inject.Inject;
+
+import su.Jalapeno.AntiSpam.Util.UI.JalapenoListActivity;
+import su.JalapenoAntiSpam.UI.SenderAdapter;
+import su.JalapenoAntiSpam.UI.SmsAdapter;
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Context;
 import android.view.Menu;
 
-public class SmsAnalyzerActivity extends Activity {
+public class SmsAnalyzerActivity extends JalapenoListActivity {
 
+	private Context _context;
+
+	@Inject
+	SmsAdapter _smsAdapter;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -17,6 +28,15 @@ public class SmsAnalyzerActivity extends Activity {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.sms_analyzer, menu);
 		return true;
+	}
+	
+	private void Init() {
+		_context = this.getApplicationContext();
+		LoadList();
+	}
+
+	private void LoadList() {
+		setListAdapter(_smsAdapter);
 	}
 
 }
