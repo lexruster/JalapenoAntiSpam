d9e9d980dd3180bb5e67367eeff4216ef01ef644
alexus_ru <alexus_ru@localhost>
2014-02-26 17:24:15 +0400

    ???????? ?????

--- Client/Android/JalapenoAntiSpam/gen/su/jalapeno/AntiSpam/R.java
+++ Client/Android/JalapenoAntiSpam/gen/su/jalapeno/AntiSpam/R.java
@@ -230,14 +230,18 @@ containing a value of this type.
         public static final int common_signin_btn_text_normal_light=0x7f020016;
         public static final int common_signin_btn_text_pressed_dark=0x7f020017;
         public static final int common_signin_btn_text_pressed_light=0x7f020018;
-        public static final int ic_launcher=0x7f020019;
-        public static final int ic_plusone_medium_off_client=0x7f02001a;
-        public static final int ic_plusone_small_off_client=0x7f02001b;
-        public static final int ic_plusone_standard_off_client=0x7f02001c;
-        public static final int ic_plusone_tall_off_client=0x7f02001d;
-        public static final int jalapeno=0x7f02001e;
-        public static final int mail=0x7f02001f;
-        public static final int mailb=0x7f020020;
+        public static final int hbutton=0x7f020019;
+        public static final int hbutton_focused=0x7f02001a;
+        public static final int hbutton_normal=0x7f02001b;
+        public static final int hbutton_pressed=0x7f02001c;
+        public static final int ic_launcher=0x7f02001d;
+        public static final int ic_plusone_medium_off_client=0x7f02001e;
+        public static final int ic_plusone_small_off_client=0x7f02001f;
+        public static final int ic_plusone_standard_off_client=0x7f020020;
+        public static final int ic_plusone_tall_off_client=0x7f020021;
+        public static final int jalapeno=0x7f020022;
+        public static final int mail=0x7f020023;
+        public static final int mailb=0x7f020024;
     }
     public static final class id {
         public static final int action_settings=0x7f070024;
@@ -421,6 +425,8 @@ containing a value of this type.
         public static final int BaseControl_ListView=0x7f0c0006;
         public static final int BaseControl_TextHeader=0x7f0c0005;
         public static final int BaseLayout=0x7f0c0001;
+        public static final int JalapenoTheme=0x7f0c0009;
+        public static final int JalaponeThemeButton=0x7f0c000a;
         public static final int ListItem=0x7f0c0007;
     }
     public static final class styleable {
