31e8a90835762fcdb94cae482a10744f2b389a0c
alexus_ru <alexus_ru@localhost>
2014-03-31 16:27:44 +0400

    ????? CheckPoint

--- Client/Android/JalapenoAntiSpam/src/su/Jalapeno/AntiSpam/Util/Logger.java
+++ Client/Android/JalapenoAntiSpam/src/su/Jalapeno/AntiSpam/Util/Logger.java
@@ -5,17 +5,17 @@ import android.util.Log;
 public class Logger {
 	public static final boolean LOG_DEBUG = true;
 
-	public static void Debug(String lOG_TAG, String string) {
+	public static void Debug(String LOG_TAG, String string) {
 		if (LOG_DEBUG) {
-			Log.d(lOG_TAG, string);
+			Log.d(LOG_TAG, string);
 		}
 	}
 
-	public static void Error(String lOG_TAG, String string) {
-		Log.e(lOG_TAG, string);
+	public static void Error(String LOG_TAG, String string) {
+		Log.e(LOG_TAG, string);
 	}
 
-	public static void Error(String lOG_TAG, String string, Exception ex) {
-		Log.e(lOG_TAG, string, ex);
+	public static void Error(String LOG_TAG, String string, Exception ex) {
+		Log.e(LOG_TAG, string, ex);
 	}
 }
