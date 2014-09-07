d41bcb88b79e2f449ac7d05f37f3212b9c73669a
alexus_ru <alexus_ru@localhost>
2014-09-08 01:12:35 +0400

    test

--- Client/Android/JalapenoAntiSpam/src/su/Jalapeno/AntiSpam/Activities/SpamerList.java
+++ Client/Android/JalapenoAntiSpam/src/su/Jalapeno/AntiSpam/Activities/SpamerList.java
@@ -32,6 +32,7 @@ public class SpamerList extends JalapenoListActivity {
 	@Override
 	public void onBackPressed() {
 		Logger.Debug(LOG_TAG, "onBackPressed");
+		Logger.Debug(LOG_TAG, "onBackPressed2");
 		UiUtils.NavigateAndClearHistory(SettingsActivity.class);
 	}
 
