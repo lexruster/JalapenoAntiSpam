e79d81e0d0f4beea54c34ce72bc9fe49c59f4b8a
Alexander <lexruster@gmail.com>
2014-09-07 15:48:58 +0400

    ????????? ????????

--- Client/Android/JalapenoAntiSpam/src/su/Jalapeno/AntiSpam/Util/Constants.java
+++ Client/Android/JalapenoAntiSpam/src/su/Jalapeno/AntiSpam/Util/Constants.java
@@ -14,8 +14,9 @@ public class Constants {
 	public final static int MIN_MESSAGE_LENGTH = 50;
 
 	// debug configs
-	public static final boolean LOG_DEBUG = false;
-	public static final boolean VIEW_DEBUG_UI = false;
+	public static final boolean FULL_DEBUG = true;
+	public static final boolean LOG_DEBUG = FULL_DEBUG;
+	public static final boolean VIEW_DEBUG_UI = FULL_DEBUG;
 	// when no internet
 	public static final boolean DEBUG_STANDALONE_MODE = false;
 }
