7b18e2302c9646edc94b541df0768f07d9656cc5
alexus_ru <alexus_ru@localhost>
2014-09-13 00:19:59 +0400

    ????

---
+++ Client/Android/Artifacts/Tools/IconSorter/IconSorter/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IconSorter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Sorter sortet=new Sorter();
+            sortet.Sort();
+
+            Console.WriteLine("Success");
+            Console.ReadKey(false);
+        }
+    }
+}
