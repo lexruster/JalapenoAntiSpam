d10c2015fda4ed3a623f7e39e085c5564fa4853d
alexus_ru <alexus_ru@localhost>
2014-10-16 17:33:48 +0400

    fix spiner 53

--- Client/Android/JalapenoAntiSpam/src/su/Jalapeno/AntiSpam/Util/UI/Spiner.java
+++ Client/Android/JalapenoAntiSpam/src/su/Jalapeno/AntiSpam/Util/UI/Spiner.java
@@ -9,13 +9,12 @@ import android.content.Context;
 public class Spiner {
 	private ProgressDialog _progressDialog;
 	private Context _context;
-
 	final String LOG_TAG = Constants.BEGIN_LOG_TAG + "Spiner";
 
 	public Spiner(Context context) {
 		_context = context;
-		_progressDialog = new ProgressDialog(_context);
-		_progressDialog.setCanceledOnTouchOutside(false);
+		CreateSpiner();
+		Logger.Debug(LOG_TAG, "Spiner construct");
 	}
 
 	public void Hide() {
@@ -30,12 +29,24 @@ public class Spiner {
 		} finally {
 			_progressDialog = null;
 		}
+		
+		Logger.Debug(LOG_TAG, "Spiner hide");
 	}
 
 	public void Show() {
 		String message = _context.getResources()
 				.getString(R.string.WaitMessage);
+		if (_progressDialog == null) {
+			CreateSpiner();
+		}
 		_progressDialog.setMessage(message);
 		_progressDialog.show();
+		Logger.Debug(LOG_TAG, "Spiner show");
+	}
+
+	private void CreateSpiner() {
+		_progressDialog = new ProgressDialog(_context);
+		_progressDialog.setCanceledOnTouchOutside(false);
+		Logger.Debug(LOG_TAG, "Spiner create");
 	}
 }
