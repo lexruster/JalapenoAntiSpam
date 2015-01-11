5fc0ad600bc3e3eb49b47c1a971504c8365802a6
alexus_ru <alexus_ru@localhost>
2015-01-11 22:20:36 +0300

    *??????? ??????????

--- Client/Android/JalapenoAntiSpam/src/su/Jalapeno/AntiSpam/Util/StrictPolicy.java
+++ Client/Android/JalapenoAntiSpam/src/su/Jalapeno/AntiSpam/Util/StrictPolicy.java
@@ -5,40 +5,32 @@ import com.google.android.vending.licensing.ResponseData;
 
 public class StrictPolicy implements Policy {
 	final String LOG_TAG = Constants.BEGIN_LOG_TAG + "StrictPolicy";
+	 public static final int NotSpam = 512;
+     public static final int Spam = 1122;
+	 public static final int Network = 582;
 
-	private int mLastResponse;
+	private int smsResp;
 	public ResponseData Response;
 
 	public StrictPolicy() {
-		// Set default policy. This will force the application to check the
-		// policy on launch.
-		mLastResponse = Policy.RETRY;
+		smsResp = Network;
 	}
 
-	/**
-	 * Process a new response from the license server. Since we aren't
-	 * performing any caching, this equates to reading the LicenseResponse. Any
-	 * ResponseData provided is ignored.
-	 * 
-	 * @param response
-	 *            the result from validating the server response
-	 * @param rawData
-	 *            the raw server response data
-	 */
 	public void processServerResponse(int response, ResponseData rawData) {
-		String info = String.format("processServerResponse response=%s extras=%s", response, rawData != null ? rawData.extra : "null");
+		String info = String.format("processServerResponse ResponseCode=%s extras=%s, String=%s",
+				response, 
+				rawData != null ? rawData.extra : "null",
+				rawData != null ? rawData.toString() : "null");
 		Logger.Debug(LOG_TAG, info);
-		mLastResponse = response;
+		if (rawData != null) {
+			Logger.Debug(LOG_TAG, rawData.toString());
+		}
+		Logger.Debug(LOG_TAG, info);
+		smsResp = response;
 		Response = rawData;
 	}
 
-	/**
-	 * {@inheritDoc}
-	 * 
-	 * This implementation allows access if and only if a LICENSED response was
-	 * received the last time the server was contacted.
-	 */
 	public boolean allowAccess() {
-		return (mLastResponse == Policy.LICENSED);
+		return (smsResp*2 == NotSpam);
 	}
 }
